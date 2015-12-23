1719cf0fcec161170360fb0087c90066d32b633e
Satish Bharose <bharoseambs@gmail.com>
2015-12-23 07:28:52 +0530

    First Change

---
+++ JavaRepoProject/one/two/three/main.java
@@ -0,0 +1,13 @@
+package one.two.three;
+
+public class main extends submain{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+             System.out.println("Main Class");
+             
+             submain sb = new submain();
+             	sb.display();
+	}
+
+}
---
+++ JavaRepoProject/one/two/three/submain.java
@@ -0,0 +1,10 @@
+package one.two.three;
+
+public class submain {
+
+	void display(){
+		
+		System.out.println("Submain Class");
+	}
+	
+}
